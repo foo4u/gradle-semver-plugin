c80f2340eb53073c79d84860ac4facc7ab5c502d
Scott Rossillo <scott@rossillo.net>
2013-06-10 14:50:19 -0400

    Reduced output verbosity

--- src/main/groovy/io/fugu/gradle/plugns/version/SemanticVersionPlugin.groovy
+++ src/main/groovy/io/fugu/gradle/plugns/version/SemanticVersionPlugin.groovy
@@ -2,6 +2,8 @@ package io.fugu.gradle.plugns.version
 
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
 
 /**
  * Provides a semantic version plugin for Gradle.
@@ -11,13 +13,15 @@ import org.gradle.api.Plugin
  */
 class SemanticVersionPlugin implements Plugin<Project> {
 
+	private final Logger logger = Logging.getLogger("semver")
+
 	void apply(Project project) {
 
 		project.setVersion(new SemanticVersion())
 
-		project.task('release') << { println "Releasing $project.name version $project.version" }
+		project.task('release') << { logger.lifecycle("Releasing $project.name version $project.version") }
 
-		project.task('printVersion') << { println "Project version is $project.version" }
+		project.task('printVersion') << { println "$project.version" }
 
 		project.gradle.taskGraph.whenReady { taskGraph ->
 			project.version.release = taskGraph.hasTask(':release')
